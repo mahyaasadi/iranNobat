a2b404a57355f12acfec28b37c1272523c0859bb
mahyaasadi <mahyaasadi378@gmail.com>
2023-06-08 12:38:07 +0430

    changes in dashboard main page

--- login/components/DashboardLayout/DashboardLayout.js
+++ login/components/DashboardLayout/DashboardLayout.js
@@ -1,38 +1,28 @@
 "use client"; // This is a client component
-// import Header from "../Header";
-// import Sidebar from "../Sidebar";
-// import Content from "../Content";
-import Image from "next/image";
-import "../../src/assets/img/favicon.png";
-import "../../src/assets/css/bootstrap.rtl.min.css";
-import "../../src/assets/css/font-awesome.min.css";
-import "../../src/assets/css/feathericon.min.css";
-import "../../src/assets/plugins/morris/morris.css";
+import Header from "../Header/Header";
+import Sidebar from "../Sidebar/Sidebar";
+import DashboardContent from "../Content/DashboardContent";
+import axios from "axios";
 import "../../src/assets/css/style.css";
-import logo from "../../src/assets/img/logo.png";
-import logoSmall from "../../src/assets/img/logo-small.png";
 
 export default function DashboardLayout() {
-  return (
-    <div class="main-wrapper">
-      {/* <!-- Header --> */}
-      <div class="header">
-        {/* <!-- Logo --> */}
-        <div class="header-left">
-          <a href="index.html" class="logo">
-            <Image src={logo} alt="Logo" width="80" height="80" />
-          </a>
-          <a href="index.html" class="logo logo-small">
-            <Image src={logoSmall} alt="Logo" width="30" height="30" />
-          </a>
-        </div>
-        {/* <!-- /Logo --> */}
+ 
 
-        {/* <Header />
-      <Content>
-        <Sidebar />
-      </Content> */}
+  return (
+    <div className="main-wrapper">
+      <Header />
+      <Sidebar />
+      <div className="p-40">
+        <DashboardContent />
       </div>
     </div>
   );
 }
+
+// Page.getLayout = function getLayout(page) {
+//   return (
+//     <Layout>
+//       <NestedLayout>{page}</NestedLayout>
+//     </Layout>
+//   )
+// }
